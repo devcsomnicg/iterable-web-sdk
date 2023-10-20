91a9a93ee05f6fcb178c4daeba3a34323fa273da
Hani <devcs@omnicg.com>
2023-10-20 18:19:12 +0530

    initial setup

--- react-example/src/index.tsx
+++ react-example/src/index.tsx
@@ -14,6 +14,7 @@ import styled from 'styled-components';
 import LoginForm from 'src/components/LoginForm';
 
 import { UserProvider } from 'src/context/Users';
+import { createRoot } from 'react-dom/client';
 
 const Wrapper = styled.div`
   display: flex;
@@ -61,7 +62,8 @@ const HomeLink = styled(Link)`
     }
   );
 
-  ReactDOM.render(
+  const root = createRoot(document.getElementById('root'));
+  root.render(
     <BrowserRouter>
       <Wrapper>
         <UserProvider>
